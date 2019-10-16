08ca12c65eabb6d0f4b276e0b126bc69d5b73f7b
Miguel Fernandez <miguelrf95@gmail.com>
2019-10-16 16:51:10 +0100

    Add script for coRoutine testing

--- Assets/Scripts/CoRoutine_Test.cs
+++ Assets/Scripts/CoRoutine_Test.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CoRoutine_Test : MonoBehaviour
 {
+    //Class variables
+    private Coroutine coRouSayHello;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        coRouSayHello = StartCoroutine(SayHello());
+        StartCoroutine(CountTill30());
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private IEnumerator SayHello()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(4);
+
+        while (true)
+        {
+            Debug.Log("Hello");
+            yield return wfs;
+        }
+    }
+
+    private IEnumerator CountTill30()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(1);
+        int n = 0;
+
+        while (true)
+        {
+            Debug.Log(n);
+            n++;
+            if (n > 30)
+            {
+                StopCoroutine(coRouSayHello);
+            }
+            yield return wfs;
+        }
     }
 }
